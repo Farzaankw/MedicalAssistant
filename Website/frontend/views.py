4e96fd6fe29cb4a6b5df611c32cec264ce8b8c72
Farzaankw <92130577+Farzaankw@users.noreply.github.com>
2021-11-07 13:38:25 -0800

    Working on about us section

--- Website/frontend/views.py
+++ Website/frontend/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request, *args, **kwargs):
-    return render(request, 'frontend/index.html')
+    return render(request, 'frontend/index.html'),
+
+def about(request):
+    return render(request, 'about.html', {})
